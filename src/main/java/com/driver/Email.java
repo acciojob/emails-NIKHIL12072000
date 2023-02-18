6cac4825c0a67f924db4196f9ee04fa638d978cb
Nikhil <dachepallinikhil@gmail.com>
2023-02-18 18:40:40 +0530

    Tasks Completed

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,19 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        boolean c1=false,c2=false,c3=false,c4=false,c5=false;
+        if(oldPassword.equals(newPassword)){
+            if(newPassword.length()>=8){
+                c1=true;
+                for(int i=0;i<newPassword.length();i++){
+                    int x=(int)newPassword.charAt(i);
+                    if(x>=65 && x<=90) c2=true;
+                    else if(x>=97 && x<=122) c3=true;
+                    else if(x>=48 && x<=57) c4=true;
+                    else c5=true;
+                }
+            }
+            if(c1 && c2 && c3 && c4 && c5) password=newPassword;
+        }
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 public class Workspace extends Gmail{
 
@@ -12,12 +13,13 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+        super(emailId,Integer.MAX_VALUE);
+        calendar=new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+        calendar.add(meeting);
     }
 
     public int findMaxMeetings(){
@@ -25,6 +27,23 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-
+        if(calendar.size()==0) return 0;
+        int count=1;
+        LocalTime start,end,next_start;
+        Collections.sort(calendar, new Comparator<Meeting>() {
+            @Override
+            public int compare(Meeting o1, Meeting o2) {
+                if(o1.getStartTime().compareTo(o2.getStartTime())<0) return -1;
+                else if(o1.getStartTime().compareTo(o2.getStartTime())>0) return 1;
+                else return 0;
+            }
+        });
+        for(int i=0;i<calendar.size()-1;i++){
+            start=calendar.get(i).getStartTime();
+            end=calendar.get(i).getEndTime();
+            next_start=calendar.get(i+1).getStartTime();
+            if(start.compareTo(next_start)<0 && next_start.compareTo(end)>0) count++;
+        }
+        return count;
     }
 }
